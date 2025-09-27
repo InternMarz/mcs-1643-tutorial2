6c857bf5cbc12723a5e22c66c786853389c7471d
InternMarz <rdemarcus323@gmail.com>
2025-09-27 18:19:23 -0400

    video (3)

--- Assets/_Scripts/Paddle.cs
+++ Assets/_Scripts/Paddle.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    public float speed = 12.0f;
+    public float speed = 30.0f;
+    public float maxZ = 12.0f;
     public KeyCode upKey;
     public KeyCode downKey;
     // Update is called once per frame
@@ -13,10 +14,18 @@ public class Paddle : MonoBehaviour
         if (Input.GetKey(upKey))
         {
             transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+            if (transform.position.z > maxZ)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+            }
         }
         else if (Input.GetKey(downKey))
         {
             transform.position += new Vector3(0, 0, -1 * speed * Time.deltaTime);
+            if (transform.position.z < -1 * maxZ)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, -1 * maxZ);
+            }
         }
     }
 }
